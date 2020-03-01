f318bd67bf6ae8f1b79ddf8550a251f8d8b8a953
Vincent NGUYEN VAN NGUYEN <vincent.n@hotmail.fr>
2020-03-01 20:00:50 +0100

    bootstrap and more

--- js/map.js
+++ js/map.js
@@ -1,15 +1,15 @@
 mapboxgl.accessToken =
   "pk.eyJ1IjoidmljaGkwNiIsImEiOiJjazc2YmRyeDUxM3VsM2d0MDh6ZHBnc2hqIn0.u1mi0Gf1Rs7_-HaiHsKTCw";
+
 var map = new mapboxgl.Map({
   container: "map",
   style: "mapbox://styles/mapbox/dark-v10",
   zoom: 4.4,
   hash: true,
-  interactive: false,
   center: [46.54, 2.84]
 });
 
-var layerList = document.getElementById("menu");
+var layerList = document.getElementById("mapMenu");
 var inputs = layerList.getElementsByTagName("input");
 
 function switchLayer(layer) {
@@ -21,20 +21,8 @@ for (var i = 0; i < inputs.length; i++) {
   inputs[i].onclick = switchLayer;
 }
 
-function letsInteract() {
-    anime({
-        targets: '.duration-demo .el',
-        translateX: 250,
-        duration: 3000
-      });
-
-
- /* var map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/dark-v10",
-    zoom: 4.4,
-    hash: true,
-    interactive: true,
-    center: [46.54, 2.84]
-  });*/
+function toggle_map() {
+  $("#button_toggle").hide();
+  $("#map").toggle("slide");
+  $("#mapMenu").toggle("slide");
 }
