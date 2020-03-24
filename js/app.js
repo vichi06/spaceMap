69567d98cebf40b45c4facb4d22a0a0536d17ee5
LAY Frederic <frederic.lay@etu.parisdescartes.fr>
2020-03-24 17:32:06 +0100

    front carre

--- js/app.js
+++ js/app.js
@@ -71,108 +71,133 @@ form.addEventListener("submit", e => {
 */
 
 form.addEventListener("submit", e => {
-  e.preventDefault();
-  var players = db.collection("players");
-  var found = false;
-  if (document.getElementById("guestCheckBox").checked == true) {
-    nbPlayer++;
-    found = true;
-    let li = document.createElement("li");
-    let name = document.createElement("span");
-    let score = document.createElement("span");
-    let cross = document.createElement("div");
+	e.preventDefault();
+	var players = db.collection("players");
+	var found = false;
+	if (document.getElementById("guestCheckBox").checked == true) {
+		nbPlayer++;
+		found = true;
+		let li = document.createElement("li");
+		let name = document.createElement("span");
+		let score = document.createElement("span");
+		let cross = document.createElement("div");
 
-    li.setAttribute("data-id", "0");
-    name.textContent = form.username.value;
-    score.textContent = "highest score: " + "none";
-    cross.textContent = "x";
+		li.setAttribute("data-id", "0");
+		name.textContent = form.username.value;
+		score.textContent = "highest score: " + "none";
+		cross.textContent = "x";
 
-    li.appendChild(name);
-    li.appendChild(score);
-    li.appendChild(cross);
+		li.appendChild(name);
+		li.appendChild(score);
+		li.appendChild(cross);
 
-    cafeList.appendChild(li);
+		cafeList.appendChild(li);
 
-    cross.addEventListener("click", e => {
-      nbPlayer--;
-      e.parentNode.removeChild(e);
-    });
-  } else {
-    players.get().then(function(querySnapsot) {
-      querySnapsot.forEach(function(doc) {
-        if (
-          form.username.value == doc.data().username &&
-          form.password.value == doc.data().password
-        ) {
-          found = true;
-          nbPlayer++;
+		cross.addEventListener("click", e => {
+			nbPlayer--;
+			e.parentNode.removeChild(e);
+		});
+	} else {
+		players.get().then(function(querySnapsot) {
+			querySnapsot.forEach(function(doc) {
+				if (
+					form.username.value == doc.data().username &&
+					form.password.value == doc.data().password
+				) {
+					found = true;
+					nbPlayer++;
 
-          let li = document.createElement("li");
-          let name = document.createElement("span");
-          let score = document.createElement("span");
-          let cross = document.createElement("div");
+					let li = document.createElement("li");
+					let name = document.createElement("span");
+					let score = document.createElement("span");
+					let cross = document.createElement("div");
 
-          li.setAttribute("data-id", doc.id);
-          name.textContent = doc.data().username;
-          score.textContent = "highest score: " + doc.data().record;
-          cross.textContent = "x";
+					li.setAttribute("data-id", doc.id);
+					name.textContent = doc.data().username;
+					score.textContent = "highest score: " + doc.data().record;
+					cross.textContent = "x";
 
-          li.appendChild(name);
-          li.appendChild(score);
-          li.appendChild(cross);
+					li.appendChild(name);
+					li.appendChild(score);
+					li.appendChild(cross);
 
-          cafeList.appendChild(li);
+					cafeList.appendChild(li);
 
-          cross.addEventListener("click", e => {
-            nbPlayer--;
-            cross.parentNode.parentNode.removeChild(li);
-          });
-        }
-      });
-    });
-  }
-  if (!found) {
-    alert("mauvais identifiant ou mot de passe");
-  }
+					cross.addEventListener("click", e => {
+						nbPlayer--;
+						cross.parentNode.parentNode.removeChild(li);
+					});
+				}
+			});
+		});
+	}
+	if (!found) {
+		alert("mauvais identifiant ou mot de passe");
+	}
 });
 
 var guestCheckbox = document.querySelector("input[name=guestCheckBox]");
 
 guestCheckbox.addEventListener("change", function() {
-  var password = document.getElementById("password");
-  if (this.checked) {
-    password.disabled = true;
-    password.value = "";
-  } else {
-    password.disabled = false;
-  }
+	var password = document.getElementById("password");
+	if (this.checked) {
+		password.disabled = true;
+		password.value = "";
+	} else {
+		password.disabled = false;
+	}
 });
 
 function play() {
-  if (nbPlayer > 0 && nbPlayer < 5) {
-    form.parentNode.removeChild(form);
-    cafeList.disabled = true;
-    var playButton = document.getElementById("playButton");
-    playButton.parentNode.removeChild(playButton);
-  } else {
-    alert("You need at least 1 player and maximum 4");
-  }
+	if (nbPlayer > 0 && nbPlayer < 5) {
+		form.parentNode.removeChild(form);
+		cafeList.disabled = true;
+		var playButton = document.getElementById("playButton");
+		playButton.parentNode.removeChild(playButton);
+	} else {
+		alert("You need at least 1 player and maximum 4");
+	}
 }
 
 var selectSatellite = document.querySelector("button[name=selectSatellite]");
 
 selectSatellite.addEventListener("click", e => {
-  alert("you have selected" + e.name);
+	alert("you have selected" + e.name);
 });
 
 //Leaflet
 var map = L.map("carte").setView([48.8534, 2.3488], 6);
 L.tileLayer(
-  "https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
-  {
-    maxZoom: 20
-  }
+	"https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png",
+	{
+		maxZoom: 20
+	}
 ).addTo(map);
 
-var btn_play = document.getElementById("btn_play");
-btn_play.addEventListener("click", e => {});
+const issIcon = L.icon({
+	iconUrl: "iss200.png",
+	iconSize: [50, 32],
+	iconAnchor: [25, 16]
+});
+
+const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
+
+const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
+
+let firstTime = true;
+
+async function getISS() {
+	const response = await fetch(api_url);
+	const data = await response.json();
+	const { latitude, longitude } = data;
+
+	marker.setLatLng([latitude, longitude]);
+	if (firstTime) {
+		map.setView([latitude, longitude], 2);
+		firstTime = false;
+	}
+	document.getElementById("lat").textContent = latitude.toFixed(2);
+	document.getElementById("lon").textContent = longitude.toFixed(2);
+}
+
+getISS();
