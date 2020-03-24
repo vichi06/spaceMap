3ef37ecae54baa4f0a4e299365cf965a367b568d
Vincent NGUYEN VAN NGUYEN <vincent.n@hotmail.fr>
2020-03-24 21:16:01 +0100

    satellites

--- js/map.js
+++ js/map.js
@@ -7,12 +7,13 @@ L.tileLayer(
   }
 ).addTo(map);
 
+// ISS
+/*
 const issIcon = L.icon({
   iconUrl: "iss200.png",
   iconSize: [50, 32],
   iconAnchor: [25, 16]
 });
-
 const marker = L.marker([0, 0], { icon: issIcon }).addTo(map);
 
 const api_url = "https://api.wheretheiss.at/v1/satellites/25544";
@@ -32,51 +33,70 @@ async function getISS() {
   document.getElementById("lat").textContent = latitude.toFixed(2);
   document.getElementById("lon").textContent = longitude.toFixed(2);
 }
-
 getISS();
 setInterval(getISS, 1000);
-
-var satellite;
+*/
 
 // SATELLITES
+var satellite;
+var satelliteIndia;
+var satelliteEcho1;
+var satelliteEcho2;
+var satelliteCharlie1;
+var satelliteCharlie2;
+var satelliteDelta1;
+var satelliteDelta2;
+
+var playerNumber = 1;
+
+function onMapClick(e) {
+  switch (satellite) {
+    case "India":
+      India(e);
+      break;
+    case "Echo":
+      Echo(e);
+      break;
+    case "Charlie":
+      Charlie(e);
+      break;
+    case "Delta":
+      Delta(e);
+      break;
+  }
+}
+
+map.on("click", onMapClick);
+
 function chooseSatellite(satelliteName) {
   document.getElementById("about").scrollIntoView();
   satellite = satelliteName;
-  switch (satelliteName) {
-    case "India":
-      India();
-      break;
-    case "Echo":
-      Echo();
-      break;
-    case "Charlie":
-      Charlie();
-      break;
-    case "Delta":
-      Delta();
-      break;
+}
+
+function India(e) {
+  if (satelliteIndia != undefined) {
+    map.removeLayer(satelliteIndia);
   }
 
-  /*
-    let img = document.createElement("img");
-    var src = "images/satellites/" + satelliteName + ".png";
-    img.setAttribute("src", src);
-    img.setAttribute("id", "draggable");
-    document.getElementById("mapView").appendChild(img);
-  */
+  const indiaIcon = L.icon({
+    iconUrl: "images/satellites/India.png",
+    iconSize: [50, 32]
+  });
+  const markerIndia = L.marker(e.latlng, { icon: indiaIcon }).addTo(map);
 
-  function India(e) {
-    var clickCircle1 = L.circle(e.latlng, 688888 * 3, {
-      iconUrl: "images/satellites/India.png",
-      color: "#6CFF33",
-      fillOpacity: 0,
-      opacity: 0.5
-    }).addTo(map);
-  }
+  satelliteIndia = L.circle(e.latlng, 688888 * 3, {
+    color: "#000000",
+    fillOpacity: 0,
+    opacity: 0.5
+  }).addTo(map);
+}
 
-  function Charlie() {}
+function Charlie() {}
 
-  function Echo() {}
+function Echo() {}
 
-  function Delta() {}
+function Delta() {}
+
+function nextPlayer() {
+  playerNumber++;
 }
