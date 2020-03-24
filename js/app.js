c3468f8543a0bc57432620edf26e401cd1c326b4
Vincent NGUYEN VAN NGUYEN <vincent.n@hotmail.fr>
2020-03-24 17:50:52 +0100

    fusion2

--- js/app.js
+++ js/app.js
@@ -1,169 +1,103 @@
-const cafeList = document.querySelector("#cafe-list");
-const form = document.querySelector("#add-cafe-form");
+const playerList = document.querySelector("#player-list");
+const form = document.querySelector("#add-player-form");
 
 var nbPlayer = 0;
 
-// create element and render cafe
-/*
-function renderCafe(doc) {
-	let li = document.createElement("li");
-	let name = document.createElement("span");
-	let score = document.createElement("span");
-	let cross = document.createElement("div");
+// FORMULAIRE PLAYER
+form.addEventListener("submit", e => {
+  e.preventDefault();
 
-	li.setAttribute("data-id", doc.id);
-	name.textContent = doc.data().username;
-	score.textContent = "highest score: " + doc.data().record;
-	cross.textContent = "x";
+  nbPlayer++;
+  var playerAdded = false;
+  var players = db.collection("players");
 
-	li.appendChild(name);
-	li.appendChild(score);
-	li.appendChild(cross);
+  if (nbPlayer <= 4) {
+    // GUEST PLAYER
+    if (document.getElementById("guestCheckBox").checked == true) {
+      addPlayerInList("0", form.username.value, "none");
+      playerAdded = true;
+    }
+    // REGISTERED PLAYER
+    else {
+      players.get().then(function(querySnapsot) {
+        querySnapsot.forEach(function(doc) {
+          if (
+            //VERIFICATION
+            form.username.value == doc.data().username &&
+            form.password.value == doc.data().password
+          ) {
+            addPlayerInList(doc.id, doc.data().username, doc.data().password);
+            playerAdded = true;
+          }
+        });
+      });
+    }
+  } else {
+    alert("The game supports 4 players maximum and 1 minimum");
+  }
 
-	cafeList.appendChild(li);
+  if (!playerAdded) {
+    nbPlayer--;
+    alert("No player found in database");
+  }
+});
 
-	// deleting data
-	cross.addEventListener("click", e => {
-		let id = e.target.parentElement.getAttribute("data-id");
-		db.collection("players")
-			.doc(id)
-			.delete();
-	});
+// ADD A PLAYER TO THE LIST
+function addPlayerInList(id, username, record) {
+  let li = document.createElement("li");
+  let name = document.createElement("span");
+  let score = document.createElement("span");
+  let cross = document.createElement("div");
+
+  li.setAttribute("data-id", id);
+  name.textContent = username;
+  score.textContent = "highest score: " + record;
+  cross.textContent = "x";
+
+  li.appendChild(name);
+  li.appendChild(score);
+  li.appendChild(cross);
+
+  playerList.appendChild(li);
+  cross.addEventListener("click", e => {
+    nbPlayer--;
+    cross.parentNode.parentNode.removeChild(li);
+  });
 }
-// getting data
-// db.collection("players")
-// 	.orderBy("username")
-// 	.get()
-// 	.then(snapshot => {
-// 		snapshot.docs.forEach(doc => {
-// 			console.log(doc.data());
-// 			renderCafe(doc);
-// 		});
-// 	});
-
-// realTime listener
-db.collection("players")
-	.orderBy("username")
-	.onSnapshot(snapshot => {
-		let changes = snapshot.docChanges();
-		changes.forEach(change => {
-			//console.log(change.doc.data())
-			if (change.type == "added") {
-				renderCafe(change.doc);
-			} else if (change.type == "removed") {
-				let li = cafeList.querySelector("[data-id=" + change.doc.id + "]");
-				cafeList.removeChild(li);
-			}
-		});
-	});
-
-// saving data
-form.addEventListener("submit", e => {
-	e.preventDefault();
-	db.collection("players").add({
-		username: form.username.value,
-		password: form.password.value,
-		record: 0
-	});
-	form.username.value = "";
-	form.password.value = "";
-});
-*/
-
-form.addEventListener("submit", e => {
-	e.preventDefault();
-	var players = db.collection("players");
-	var found = false;
-	if (document.getElementById("guestCheckBox").checked == true) {
-		nbPlayer++;
-		found = true;
-		let li = document.createElement("li");
-		let name = document.createElement("span");
-		let score = document.createElement("span");
-		let cross = document.createElement("div");
-
-		li.setAttribute("data-id", "0");
-		name.textContent = form.username.value;
-		score.textContent = "highest score: " + "none";
-		cross.textContent = "x";
-
-		li.appendChild(name);
-		li.appendChild(score);
-		li.appendChild(cross);
-
-		cafeList.appendChild(li);
-
-		cross.addEventListener("click", e => {
-			nbPlayer--;
-			e.parentNode.removeChild(e);
-		});
-	} else {
-		players.get().then(function(querySnapsot) {
-			querySnapsot.forEach(function(doc) {
-				if (
-					form.username.value == doc.data().username &&
-					form.password.value == doc.data().password
-				) {
-					found = true;
-					nbPlayer++;
-
-					let li = document.createElement("li");
-					let name = document.createElement("span");
-					let score = document.createElement("span");
-					let cross = document.createElement("div");
-
-					li.setAttribute("data-id", doc.id);
-					name.textContent = doc.data().username;
-					score.textContent = "highest score: " + doc.data().record;
-					cross.textContent = "x";
-
-					li.appendChild(name);
-					li.appendChild(score);
-					li.appendChild(cross);
-
-					cafeList.appendChild(li);
-
-					cross.addEventListener("click", e => {
-						nbPlayer--;
-						cross.parentNode.parentNode.removeChild(li);
-					});
-				}
-			});
-		});
-	}
-	if (!found) {
-		alert("mauvais identifiant ou mot de passe");
-	}
-});
 
 var guestCheckbox = document.querySelector("input[name=guestCheckBox]");
 
+// GUEST CHECKBOX
 guestCheckbox.addEventListener("change", function() {
-	var password = document.getElementById("password");
-	if (this.checked) {
-		password.disabled = true;
-		password.value = "";
-	} else {
-		password.disabled = false;
-	}
+  var password = document.getElementById("password");
+  if (this.checked) {
+    password.disabled = true;
+    password.value = "";
+  } else {
+    password.disabled = false;
+  }
 });
 
+// PLAY -- AFTER REGISTER PLAYERS
 function play() {
-	if (nbPlayer > 0 && nbPlayer < 5) {
-		form.parentNode.removeChild(form);
-		cafeList.disabled = true;
-		var playButton = document.getElementById("playButton");
-		playButton.parentNode.removeChild(playButton);
-	} else {
-		alert("You need at least 1 player and maximum 4");
-	}
+  if (hasCorrectNumberOfPlayers()) {
+    form.parentNode.removeChild(form);
+    playerList.disabled = true;
+    var playButton = document.getElementById("playButton");
+    playButton.parentNode.removeChild(playButton);
+  } else {
+    alert("You need at least 1 player and maximum 4");
+  }
 }
 
-var selectSatellite = document.querySelector("button[name=selectSatellite]");
-
-selectSatellite.addEventListener("click", e => {
-	alert("you have selected" + e.name);
-});
+// CHECKING NUMBER OF PLAYERS
+function hasCorrectNumberOfPlayers() {
+  if (nbPlayer > 0 && nbPlayer < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
 //Leaflet
 var map = L.map("carte").setView([48.8534, 2.3488], 6);
@@ -201,3 +135,70 @@ async function getISS() {
 }
 
 getISS();
+
+// create element and render player
+/*
+function renderplayer(doc) {
+	let li = document.createElement("li");
+	let name = document.createElement("span");
+	let score = document.createElement("span");
+	let cross = document.createElement("div");
+
+	li.setAttribute("data-id", doc.id);
+	name.textContent = doc.data().username;
+	score.textContent = "highest score: " + doc.data().record;
+	cross.textContent = "x";
+
+	li.appendChild(name);
+	li.appendChild(score);
+	li.appendChild(cross);
+
+	playerList.appendChild(li);
+
+	// deleting data
+	cross.addEventListener("click", e => {
+		let id = e.target.parentElement.getAttribute("data-id");
+		db.collection("players")
+			.doc(id)
+			.delete();
+	});
+}
+// getting data
+// db.collection("players")
+// 	.orderBy("username")
+// 	.get()
+// 	.then(snapshot => {
+// 		snapshot.docs.forEach(doc => {
+// 			console.log(doc.data());
+// 			renderplayer(doc);
+// 		});
+// 	});
+
+// realTime listener
+db.collection("players")
+	.orderBy("username")
+	.onSnapshot(snapshot => {
+		let changes = snapshot.docChanges();
+		changes.forEach(change => {
+			//console.log(change.doc.data())
+			if (change.type == "added") {
+				renderplayer(change.doc);
+			} else if (change.type == "removed") {
+				let li = playerList.querySelector("[data-id=" + change.doc.id + "]");
+				playerList.removeChild(li);
+			}
+		});
+	});
+
+// saving data
+form.addEventListener("submit", e => {
+	e.preventDefault();
+	db.collection("players").add({
+		username: form.username.value,
+		password: form.password.value,
+		record: 0
+	});
+	form.username.value = "";
+	form.password.value = "";
+});
+*/
